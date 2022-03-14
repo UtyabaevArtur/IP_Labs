600a24fa340c7a290bb6926d5fd36ce7a91bc995
Utyabaev <nordnord02@yandex.ru>
2022-03-15 00:08:50 +0500

    Add index, lab-1-1, lab-1-2 and lab-1-3

---
+++ index.php
@@ -0,0 +1,23 @@
+<p> Дата и время:</p>
+<?php
+	$d=date("d.m.Y H:i");
+	echo($d);
+?>
+
+<ul> 
+	<li> <a href='/lab-1-1.php'> lab-1-1 </a> </li>
+	<li> <a href='/lab-1-2.php'> lab-1-2 </a> </li>
+	<li> <a href='/lab-1-3.php'> lab-1-3 </a> </li>
+	<li> <a href='/lab-1-5.php'> lab-1-5 </a> </li>
+	<li> <a href='/lab-2-1.php'> lab-2-1 </a> </li>
+	<li> <a href='/lab-2-2.php'> lab-2-2 </a> </li>
+	<li> <a href='/lab-2-3.php'> lab-2-3 </a> </li>
+	<li> <a href='/lab-2-4.php'> lab-2-4 </a> </li>
+	<li> <a href='/lab-2-5.php'> lab-2-5 </a> </li>
+	<li> <a href='/lab-2-6.php'> lab-2-6 </a> </li>
+	<li> <a href='/lab-2-7.php'> lab-2-7 </a> </li>
+	<li> <a href='/lab-2-8.php'> lab-2-8 </a> </li>
+	<li> <a href='/lab-2-9.php'> lab-2-9 </a> </li>
+	<li> <a href='/lab-2-10.php'> lab-2-10 </a> </li>
+	<li> <a href='/lab-2-11.php'> lab-2-11 </a> </li>
+</ul> 
